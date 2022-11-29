7ab895d6664b7b4b441080099ff95b4ef06ad410
Maxen24 <mshubham242@gmail.com>
2022-11-30 00:22:24 +0530

    Added UC-6-Day43Problem

--- assets-20221114T041410Z-001/EmployeePayroll.js
+++ assets-20221114T041410Z-001/EmployeePayroll.js
@@ -190,4 +190,43 @@ const createEmployeePayrollData=(id)=> {
     setEmployeePayrollData(employeePayrollData);
      return employeePayrollData;
 }
+
+
+const setEmployeePayrollData = (employeePayrollData) => {
+    try {
+        employeePayrollData.name = employeePayrollObj._name;    
+}   catch (e) { 
+    setTextValue('.text-error', e);  
+    throw e;    
+}    
+employeePayrollData.profilePic = employeePayrollObj._profilePic;
+employeePayrollData.gender = employeePayrollObj._gender;
+employeePayrollData.department = employeePayrollObj._department;
+employeePayrollData.salary= employeePayrollObj._salary;
+employeePayrollData.note = employeePayrollObj._note;
+try {
+  employeePayrollData.startDate =
+    new Date(Date.parse(employeePayrollObj._startDate));
+    } catch (e) {
+         setTextValue('.date-error', e);
+        throw e;
+    }
+    alert(employeePayrollData.toString());
+}
+
+const createNewEmployeeId = () => {
+let empID= localStorage.getItem("EmployeeID");
+empID= !empID ? 1: (parseInt(empID)+1).toString();
+localStorage.setItem("EmployeeID", empID);    
+return empID; 
+}
+  
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+        if(item.checked) selItems.push(item.value);
+    });
+    return setItems;
+}
 }
